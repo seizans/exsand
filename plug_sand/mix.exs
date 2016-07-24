5f8eb262c3e38fffa646a41514b3f10fc4ca3365
Seizan Shimazaki <seizans@gmail.com>
2016-07-24 15:37:01 +0900

    plug, cowboy を deps に追加する

--- plug_sand/mix.exs
+++ plug_sand/mix.exs
@@ -10,24 +10,15 @@ defmodule PlugSand.Mixfile do
      deps: deps()]
   end
 
-  # Configuration for the OTP application
-  #
-  # Type "mix help compile.app" for more information
   def application do
-    [applications: [:logger],
-     mod: {PlugSand, []}]
+    [mod: {PlugSand, []},
+     applications: [:logger,
+                    :plug,
+                    :cowboy]]
   end
 
-  # Dependencies can be Hex packages:
-  #
-  #   {:mydep, "~> 0.3.0"}
-  #
-  # Or git/path repositories:
-  #
-  #   {:mydep, git: "https://github.com/elixir-lang/mydep.git", tag: "0.1.0"}
-  #
-  # Type "mix help deps" for more examples and options
   defp deps do
-    []
+    [{:plug, "~> 1.2.0-rc"},
+     {:cowboy, "~> 1.0"}]
   end
 end
