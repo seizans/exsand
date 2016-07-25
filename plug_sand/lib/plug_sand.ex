7450ae838a5ddc9df9774d7f80e00f1024493684
Seizan Shimazaki <seizans@gmail.com>
2016-07-25 12:00:58 +0900

    Plug に hello world させる

--- plug_sand/lib/plug_sand.ex
+++ plug_sand/lib/plug_sand.ex
@@ -4,7 +4,9 @@ defmodule PlugSand do
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
 
+    # TODO(seizans): port 番号を設定に切り出す
     children = [
+      Plug.Adapters.Cowboy.child_spec(:http, PlugSand.Router, [], port: 4000)
     ]
     opts = [strategy: :one_for_one, name: PlugSand.Supervisor]
     Supervisor.start_link(children, opts)
