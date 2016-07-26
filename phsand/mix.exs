685f7b35c6e187c19e4278c2b13ff1146a0fa5ff
Seizan Shimazaki <seizans@gmail.com>
2016-07-26 14:33:38 +0900

    Elixir 1.3 を指定する

--- phsand/mix.exs
+++ phsand/mix.exs
@@ -4,7 +4,7 @@ defmodule Phsand.Mixfile do
   def project do
     [app: :phsand,
      version: "0.0.1",
-     elixir: "~> 1.0",
+     elixir: "~> 1.3",
      elixirc_paths: elixirc_paths(Mix.env),
      compilers: [:phoenix] ++ Mix.compilers(),
      build_embedded: Mix.env == :prod,
