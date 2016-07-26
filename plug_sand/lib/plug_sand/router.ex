fd4389d0fa8586949998c8c5099073273086f86b
Seizan Shimazaki <seizans@gmail.com>
2016-07-26 09:32:45 +0900

    application/json 以外の media type を受け付けない

--- plug_sand/lib/plug_sand/router.ex
+++ plug_sand/lib/plug_sand/router.ex
@@ -2,6 +2,8 @@ defmodule PlugSand.Router do
   use Plug.Router
   use Plug.ErrorHandler
 
+  require Logger
+
   plug Plug.Logger
   plug Plug.Parsers,
     parsers: [:json],
@@ -24,8 +26,14 @@ defmodule PlugSand.Router do
     send_resp(conn, 404, "oops")
   end
 
+  defp handle_errors(conn, %{kind: :error, reason: %Plug.Parsers.UnsupportedMediaTypeError{media_type: media_type}}) do
+    Logger.info("#{media_type} is unsupported media type")
+    body = Poison.encode!(%{error: "#{media_type} is unsupported media type"})
+    send_resp(conn, conn.status, body)
+  end
   defp handle_errors(conn, %{kind: _kind, reason: _reason, stack: _stack} = params) do
-    IO.inspect params
-    send_resp(conn, conn.status, "Something went wrong")
+    Logger.info(params)
+    body = Poison.encode!(%{error: "Something went wrong"})
+    send_resp(conn, conn.status, body)
   end
 end
