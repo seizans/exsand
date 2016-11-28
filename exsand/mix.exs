455d1fc495594ad309ff04b7807f79983e98d501
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2016-11-28 22:38:39 +0900

    deps をアップデートした (動くとは言っていない)

--- exsand/mix.exs
+++ exsand/mix.exs
@@ -4,7 +4,7 @@ defmodule Exsand.Mixfile do
   def project do
     [app: :exsand,
      version: "0.0.1",
-     elixir: "~> 1.2",
+     elixir: "~> 1.3",
      test_coverage: [tool: Coverex.Task, log: :debug],
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
@@ -20,16 +20,15 @@ defmodule Exsand.Mixfile do
 
   # Type "mix help deps" for more examples and options
   defp deps do
-    [{:exrm, "~> 1.0.3"},
-     {:credo, "~> 0.3.11"},
-     {:coverex, "~> 1.4.8", only: :test},
-     {:httpoison, "~> 0.8.2", only: [:dev, :test]},
+    [{:credo, "~> 0.5.3"},
+     {:coverex, "~> 1.4.10", only: :test},
+     {:httpoison, "~> 0.10.0", only: [:dev, :test]},
      {:poolboy, "~> 1.5.1"},
      {:eredis, "~> 1.0.8"},
-     {:jsone, "~> 1.2.1"},
+     {:jsone, "~> 1.4.0"},
      {:ranch, override: true, github: "ninenines/ranch", ref: "master"},
      {:cowlib, override: true, github: "ninenines/cowlib", ref: "master"},
-     {:gun, github: "ninenines/gun", ref: "d88f3675dba9e066ef339789d8c223358f744aac", only: [:dev, :test]},
-     {:cowboy, github: "ninenines/cowboy", ref: "b7d666cfc746f55b0a72ef8d37f703885099daf7"}]
+     {:gun, github: "ninenines/gun", ref: "c747a613ec28c4835b33ffd924a390ee55f3cf03", only: [:dev, :test]},
+     {:cowboy, github: "ninenines/cowboy", ref: "7b248e5163fd852d6defe967318da849433dadb1"}]
   end
 end
