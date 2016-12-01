e18e3336b73196fb32484cbf85c755717893a817
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2016-12-01 14:14:55 +0900

    Poison 使うまで

--- cow/lib/cow/hello_handler.ex
+++ cow/lib/cow/hello_handler.ex
@@ -2,7 +2,10 @@ defmodule Cow.HelloHandler do
   @behaviour :cowboy_handler
 
   def init(req, opts) do
-    req2 = :cowboy_req.reply(200, [{"content-type", "text/plain"}], "Hello", req)
+    headers = [{"content-type", "application/json"}]
+    body = %{foo: "hello", bar: 35, baz: true, mado: nil, homu: ""}
+           |> Poison.encode!()
+    req2 = :cowboy_req.reply(200, headers, body, req)
     {:ok, req2, opts}
   end
 end
