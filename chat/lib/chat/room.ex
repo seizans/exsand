f35ee61e6abdf117bc1c95c7888a1371cf29db0e
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2016-12-13 19:37:33 +0900

    publish が動かない

--- chat/lib/chat/room.ex
+++ chat/lib/chat/room.ex
@@ -1,6 +1,15 @@
 defmodule Chat.Room do
   use GenServer
 
+  def publish(room_name, message) do
+    IO.puts("BBBBBBBBBBBB")
+    GenServer.cast(via_tuple(room_name), {:publish, message})
+  end
+
+  def join_room(room_name, _user_name, pid) do
+    GenServer.cast(via_tuple(room_name), {:join_room, pid})
+  end
+
   def add_message(room_name, message) do
     GenServer.cast(via_tuple(room_name), {:add_message, message})
   end
@@ -26,6 +35,16 @@ defmodule Chat.Room do
     {:reply, messages, messages}
   end
 
+  def handle_cast({:join_room, pid}, pids) do
+    {:noreply, [pid | pids]}
+  end
+  def handle_cast({:publish, message}, pids) do
+    IO.inspect(message)
+    IO.inspect(pids)
+    for pid <- pids, do: send(pid, {:broadcast, message})
+    IO.puts("AAAAAAAAAA")
+    {:noreply, pids}
+  end
   def handle_cast({:add_message, message}, messages) do
     {:noreply, [message | messages]}
   end
--- chat/lib/chat/ws_handler.ex
+++ chat/lib/chat/ws_handler.ex
@@ -27,22 +27,26 @@ defmodule Chat.WsHandler do
     IO.puts(room_name)
     IO.puts(player_id)
     IO.puts(comment)
-    {:reply, {:text, "#{comment} ACCEPTED"}, req, state}
+    Chat.Room.publish(room_name, comment)
+    {:noreply, req, state}
   end
   defp handle(%{"player_id" => player_id,
                 "room_name" => room_name}, req, state) do
     IO.puts(room_name)
     # TODO(seizans): 本当はこのユーザー処理は connect 時にやるべき
     IO.puts(player_id)
-    # TODO(seizans): Room に登録する
+    :ok = Chat.Room.join_room(room_name, player_id, self())
     {:reply, {:text, "Hello #{player_id}"}, req, state}
   end
   defp handle(%{"room_name" => room_name}, req, state) do
     IO.puts("Room name: #{room_name}")
-    {:ok, _pid} = Chat.RoomSupervisor.start_room(room_name)
     {:reply, {:text, "#{room_name} CREATED"}, req, state}
   end
 
+  def websocket_info({:broadcast, message}, req, state) do
+    IO.inspect(message)
+    {:reply, {:text, message}, req, state}
+  end
   def websocket_info(out_frame, req, state) do
     IO.inspect(out_frame)
     {:stop, req, state}
