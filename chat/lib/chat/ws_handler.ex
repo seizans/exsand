2356f83e00623d7459e9a100db95bc44d6678a9a
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2016-12-13 18:46:18 +0900

    ws payload を json にする

--- chat/lib/chat/ws_handler.ex
+++ chat/lib/chat/ws_handler.ex
@@ -6,25 +6,34 @@ defmodule Chat.WsHandler do
     {:cowboy_websocket, req, %{}}
   end
 
-  def websocket_handle({:text, "room_name: " <> room_name}, req, state) do
-    IO.puts("Room name: #{room_name}")
-    {:ok, _pid} = Chat.RoomSupervisor.start_room(room_name)
-    {:reply, {:text, "#{room_name} CREATED"}, req, state}
-  end
-  def websocket_handle({:text, "player_id: " <> player_id}, req, state) do
-    # TODO(seizans): 本当はこのユーザー処理は connect 時にやるべき
-    IO.puts player_id
-    # TODO(seizans): Room に登録する
-    {:reply, {:text, "Hello #{player_id}"}, req, state}
-  end
-  def websocket_handle({:text, "comment: " <> comment}, req, state) do
-    IO.puts comment
-    {:reply, {:text, "#{comment} ACCEPTED"}, req, state}
+  def websocket_handle({:text, body}, req, state) do
+    case Poison.decode(body) do
+      {:ok, json} ->
+        handle(json, req, state)
+      {:error, reason} ->
+        IO.inspect(reason)
+    end
   end
   def websocket_handle(in_frame, req, state) do
     IO.puts("Invalid format")
     IO.inspect(in_frame)
     {:stop, req, state}
+  end
+
+  defp handle(%{"room_name" => room_name}, req, state) do
+    IO.puts("Room name: #{room_name}")
+    {:ok, _pid} = Chat.RoomSupervisor.start_room(room_name)
+    {:reply, {:text, "#{room_name} CREATED"}, req, state}
+  end
+  defp handle(%{"player_id" => player_id}, req, state) do
+    # TODO(seizans): 本当はこのユーザー処理は connect 時にやるべき
+    IO.puts player_id
+    # TODO(seizans): Room に登録する
+    {:reply, {:text, "Hello #{player_id}"}, req, state}
+  end
+  defp handle(%{"comment" => comment}, req, state) do
+    IO.puts comment
+    {:reply, {:text, "#{comment} ACCEPTED"}, req, state}
   end
 
   def websocket_info(out_frame, req, state) do
--- chat/priv/static/app.js
+++ chat/priv/static/app.js
@@ -7,18 +7,21 @@ ws.onmessage = function(e) {console.log(e)};
 var create_room = function create_room() {
     var room_name = document.getElementById("room_name").value;
     console.log("creating room: " + room_name);
-    ws.send('room_name: ' + room_name);
+    var payload = JSON.stringify({room_name: room_name});
+    ws.send(payload);
 };
 
 var ws_start = function ws_start() {
     var player_id = document.getElementById("player_id").value;
     console.log(player_id);
     // 本当は header に player_id 入れて送りたい
-    ws.send('player_id: ' + player_id);
+    var payload = JSON.stringify({player_id: player_id})
+    ws.send(payload);
 };
 
 
 var send_comment = function send_comment() {
     var comment = document.getElementById("comment").value;
-    ws.send('comment: ' + comment);
+    var payload = JSON.stringify({comment: comment})
+    ws.send(payload);
 };
