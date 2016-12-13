7fc133a873e0a4616e9365f2747b637dafa4d200
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2016-12-13 13:09:43 +0900

    軽く整理する

--- chat/lib/chat/ws_handler.ex
+++ chat/lib/chat/ws_handler.ex
@@ -3,7 +3,7 @@ defmodule Chat.WsHandler do
 
   def init(req, _opts) do
     IO.puts "WebSocket connected"
-    {:cowboy_websocket, req, %{count: 0}}
+    {:cowboy_websocket, req, %{}}
   end
 
   def websocket_handle({:text, "player_id: " <> player_id}, req, state) do
@@ -14,22 +14,12 @@ defmodule Chat.WsHandler do
     IO.puts comment
     {:reply, {:text, "#{comment} ACCEPTED"}, req, state}
   end
-  def websocket_handle({:text, "inc"}, req, %{count: count} = state) do
-    new_count = count + 1
-    IO.puts "Now the count is increased to #{new_count}"
-    {:reply, {:text, Integer.to_string(new_count)},
-     req, %{state | count: new_count}}
-  end
-  def websocket_handle({:text, "reset_count"}, req, state) do
-    IO.puts "Count is reset to 0"
-    {:reply, {:text, "0"}, req, %{state | count: 0}}
-  end
-  def websocket_handle(_in_frame, req, state) do
-    IO.puts("Only 'inc' or 'reset_count' can be accepted.")
+  def websocket_handle(in_frame, req, state) do
+    IO.puts("Invalid format")
+    IO.inspect(in_frame)
     {:stop, req, state}
   end
 
-  # TODO(seizans): タイマーでメッセージパッシングして stop するサンプル置いておく
   def websocket_info(out_frame, req, state) do
     IO.inspect(out_frame)
     {:stop, req, state}
