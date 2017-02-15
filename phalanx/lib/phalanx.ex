3fd757c4c4dad2c26fd7faffc69bf2b1246b5c53
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2017-02-15 21:30:28 +0900

    mix new --sup phalanx

---
+++ phalanx/lib/phalanx.ex
@@ -0,0 +1,18 @@
+defmodule Phalanx do
+  @moduledoc """
+  Documentation for Phalanx.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> Phalanx.hello
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
