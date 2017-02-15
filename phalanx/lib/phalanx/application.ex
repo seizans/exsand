c84f439db153c79dbd0dd2918f9d5f389ccd6c18
Seizan Shimazaki <seizan.shimazaki@aktsk.jp>
2017-02-15 21:33:18 +0900

    余分を除去する

--- phalanx/lib/phalanx/application.ex
+++ phalanx/lib/phalanx/application.ex
@@ -1,21 +1,10 @@
 defmodule Phalanx.Application do
-  # See http://elixir-lang.org/docs/stable/elixir/Application.html
-  # for more information on OTP Applications
-  @moduledoc false
-
   use Application
 
   def start(_type, _args) do
     import Supervisor.Spec, warn: false
-
-    # Define workers and child supervisors to be supervised
     children = [
-      # Starts a worker by calling: Phalanx.Worker.start_link(arg1, arg2, arg3)
-      # worker(Phalanx.Worker, [arg1, arg2, arg3]),
     ]
-
-    # See http://elixir-lang.org/docs/stable/elixir/Supervisor.html
-    # for other strategies and supported options
     opts = [strategy: :one_for_one, name: Phalanx.Supervisor]
     Supervisor.start_link(children, opts)
   end
